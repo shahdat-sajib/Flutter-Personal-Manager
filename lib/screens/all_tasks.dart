8b85652a0d95b1c105ec99fe956248b17756854c
shahdat-sajib <shahdat35-2757@diu.edu.bd>
2022-04-19 09:56:16 +0600

    task page icons added

--- lib/screens/all_tasks.dart
+++ lib/screens/all_tasks.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:task_manager/colors/app_colors.dart';
 
 class AllTasks extends StatelessWidget {
   const AllTasks({ Key? key }) : super(key: key);
@@ -7,9 +8,13 @@ class AllTasks extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Column(
         children: [
           Container(
+            padding: EdgeInsets.only(top: 60.0, left: 20.0),
+            alignment: Alignment.topLeft,
+            child: Icon(Icons.arrow_back, color: AppColors.secondaryColor,),
             width: double.maxFinite,
             height: MediaQuery.of(context).size.height/3.2,
             decoration: BoxDecoration(
@@ -21,6 +26,39 @@ class AllTasks extends StatelessWidget {
               )
             ),
           ),
+
+          Container(
+            padding: EdgeInsets.only(left: 20.0, right: 20.0),
+            child: Row(
+              children: [
+                Icon(Icons.home, color: AppColors.secondaryColor,),
+                SizedBox(width: 10.0,),
+                Container( 
+                  child: Icon(Icons.add, color: Colors.white,size: 20.0,),//this container for 2nd icon round bg color
+                  width: 25.0,
+                  height: 25.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12.5),
+                    color: Colors.black,
+                  ),
+                ),
+
+                Expanded(child: Container()), // it is to move icons right side using expanded container
+                Icon(
+                  Icons.calendar_today,
+                  color: AppColors.secondaryColor,
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+
+                Text("2", style: TextStyle(
+                  fontSize: 20.0,
+                  color: AppColors.secondaryColor,
+                ),)
+              ],
+            ),
+          )
         ],
       ),
     );
