5bb9c6d4f1f4c69ef396104ee30c5de4a6a455f1
shahdat-sajib <shahdat35-2757@diu.edu.bd>
2022-04-29 02:58:37 +0600

    some edit before database added

--- lib/screens/all_tasks.dart
+++ lib/screens/all_tasks.dart
@@ -57,7 +57,7 @@ class AllTasks extends StatelessWidget {
                     image: AssetImage("assets/header1.jpg"))),
           ),
           Container(
-            padding: EdgeInsets.only(left: 20.0, right: 20.0),
+            padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
             child: Row(
               children: [
                 Icon(
